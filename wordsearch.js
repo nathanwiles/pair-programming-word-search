9c89b3f10717638abaf95d5e7e0532f9bcab885f
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-10-04 23:54:08 -0400

    build wordsearch function.

--- wordsearch.js
+++ wordsearch.js
@@ -1,9 +1,21 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (line of horizontalJoin) {
-        if (line.includes(word)) return true
-    }
-    return false;
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  const verticalJoin = letters[0].map((col, i) =>
+    letters.map((row) => row[i]).join("")
+  );
 
-module.exports = wordSearch
+  const checkWordIn = (lineArr) => {
+    const reverseWord = word.split("").reverse().join("");
+    for (line of lineArr) {
+      if (line.includes(word)) return true;
+      if (line.includes(reverseWord)) return true;
+    }
+  };
+
+  if (checkWordIn(horizontalJoin)) return true;
+  if (checkWordIn(verticalJoin)) return true;
+
+  return false;
+};
+
+module.exports = wordSearch;
